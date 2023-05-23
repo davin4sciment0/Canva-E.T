f39424db69c608c129331a17539032997a13fe80
davin4sciment0 <davi.nascimento0907@gmail.com>
2023-05-23 12:13:12 -0300

    adição de elementos

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,23 @@
     let objeto = document.getElementById('objeto')
     let obj = objeto.getContext ('2d')
 
+    //Lua
      obj.beginPath()
      obj.arc(550, 250, 165, 0, Math.PI*2) 
      obj.fillStyle = '#F6FACB'
      obj.fill()
 
+     //Cratéra da Lua
+     obj.beginPath()
+     obj.arc(550, 250, 25, 0, Math.PI*2) 
+     obj.fillStyle = '#C5C7A7'
+     obj.fill()
+
+     //Grama
      obj.fillStyle = '#355F25'
      obj.fillRect (10, 580, 1100, 250)
      
+     //Bicicleta
      obj.beginPath()
      obj.arc(485, 370, 58, 0, Math.PI*2) 
      obj.lineWidth = 8
@@ -26,3 +35,127 @@
      obj.strokestyle = '#929594'
      obj.lineWidth = 5
      obj.stroke()
+
+     obj.beginPath()
+     obj.moveTo (685, 320)
+     obj.lineTo (660, 210)
+     obj.strokestyle = '#929594'
+     obj.lineWidth = 5
+     obj.stroke()
+
+     
+     obj.beginPath()
+     obj.moveTo (496, 290)
+     obj.lineTo (665, 230)
+     obj.strokestyle = '#929594'
+     obj.lineWidth = 5
+     obj.stroke()
+
+          
+     obj.beginPath()
+     obj.moveTo (625, 209)
+     obj.lineTo (663, 210)
+     obj.strokestyle = '#929594'
+     obj.lineWidth = 5
+     obj.stroke()
+
+    //Muitas estrelas
+     obj.beginPath() 
+     obj.arc(550, 520, 5, 0, Math.PI*2) 
+     obj.fillStyle = '#F6FACB'
+     obj.fill()
+
+     obj.beginPath()
+     obj.arc(350, 320, 4, 0, Math.PI*2) 
+     obj.fillStyle = '#F6FACB'
+     obj.fill()
+
+     obj.beginPath()
+     obj.arc(250, 550, 5, 0, Math.PI*2) 
+     obj.fillStyle = '#F6FACB'
+     obj.fill()
+
+     obj.beginPath()
+     obj.arc(50, 520, 4, 0, Math.PI*2) 
+     obj.fillStyle = '#F6FACB'
+     obj.fill()
+
+     obj.beginPath()
+     obj.arc(350, 120, 5, 0, Math.PI*2) 
+     obj.fillStyle = '#F6FACB'
+     obj.fill()
+
+     obj.beginPath()
+     obj.arc(250, 220, 4, 0, Math.PI*2) 
+     obj.fillStyle = '#F6FACB'
+     obj.fill()
+
+     obj.beginPath()
+     obj.arc(155, 100, 4, 0, Math.PI*2) 
+     obj.fillStyle = '#F6FACB'
+     obj.fill()
+
+     obj.beginPath()
+     obj.arc(155, 100, 5, 0, Math.PI*2) 
+     obj.fillStyle = '#F6FACB'
+     obj.fill()
+
+     obj.beginPath()
+     obj.arc(155, 250, 4, 0, Math.PI*2) 
+     obj.fillStyle = '#F6FACB'
+     obj.fill()
+
+     obj.beginPath()
+     obj.arc(175, 350, 4, 0, Math.PI*2) 
+     obj.fillStyle = '#F6FACB'
+     obj.fill()
+
+     obj.beginPath()
+     obj.arc(275, 450, 5, 0, Math.PI*2) 
+     obj.fillStyle = '#F6FACB'
+     obj.fill()
+
+     obj.beginPath()
+     obj.arc(875, 450, 5, 0, Math.PI*2) 
+     obj.fillStyle = '#F6FACB'
+     obj.fill()
+
+     obj.beginPath()
+     obj.arc(820, 550, 5, 0, Math.PI*2) 
+     obj.fillStyle = '#F6FACB'
+     obj.fill()
+
+     obj.beginPath()
+     obj.arc(910, 250, 5, 0, Math.PI*2) 
+     obj.fillStyle = '#F6FACB'
+     obj.fill()
+
+     obj.beginPath()
+     obj.arc(825, 200, 5, 0, Math.PI*2) 
+     obj.fillStyle = '#F6FACB'
+     obj.fill()
+
+     obj.beginPath()
+     obj.arc(910, 350, 5, 0, Math.PI*2) 
+     obj.fillStyle = '#F6FACB'
+     obj.fill()
+
+     obj.beginPath()
+     obj.arc(845, 350, 5, 0, Math.PI*2) 
+     obj.fillStyle = '#F6FACB'
+     obj.fill()
+
+     obj.beginPath()
+     obj.arc(925, 150, 5, 0, Math.PI*2) 
+     obj.fillStyle = '#F6FACB'
+     obj.fill()
+
+     obj.beginPath()
+     obj.arc(775, 150, 5, 0, Math.PI*2) 
+     obj.fillStyle = '#F6FACB'
+     obj.fill()
+
+     obj.beginPath()
+     obj.arc(875, 100, 5, 0, Math.PI*2) 
+     obj.fillStyle = '#F6FACB'
+     obj.fill()
